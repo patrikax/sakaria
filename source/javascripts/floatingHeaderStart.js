379c339e04020b5ffa78c7573375f4120005168a
patrikax <patrik.axelsson@gmail.com>
2017-08-14 00:46:29 +0200

    overwrite test

---
+++ source/javascripts/floatingHeaderStart.js
@@ -0,0 +1,31 @@
+//=require "jquery"
+
+var yPos;
+var works = $( ".works" );
+var worksPos = works.position();
+
+$(document).ready( function() {
+  updateY();
+  $('#scrollmenu').addClass( 'absolute' );
+  console.log("works position: "+worksPos.top);
+
+} );
+
+
+function updateY(){
+  yPos = scrollY;
+ 
+  //checkY();
+}
+
+
+$(window).scroll(function(){
+  updateY();
+  if(yPos>worksPos.top){
+    $('#scrollmenu').removeClass( 'invisible' );
+  }
+  else{
+    $('#scrollmenu').addClass( 'invisible' );
+  }
+
+});
